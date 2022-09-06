cea40de4a9d928f659a99e00ec2e5b4db61430e3
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-09-06 19:52:10 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+// ----------------------------
+// ---------------------------- Display Uniform Resource Locator (URL)
+// ----------------------------
+
+{
+  let url = window.document.URL;
+  document.getElementById("demo").innerHTML = url;
+}
+
+// ----------------------------
+// ----------------------------  Nested Functions
+// ----------------------------
+
+/* 
+All functions have access to the global scope.  
+all functions have access to the scope "above" them.
+*/
+
+{
+  // w obiekcie {demo_2}(komp) wywołaj funkcję --> add();
+  document.getElementById("demo_2").innerHTML = add();
+
+  function add() {
+    // powołaj obiekt(prim) --> {countera}
+    let counter = 0;
+
+    // zadeklaruj funkcję --> plus()
+    function plus() {
+      //zwiększ --> {counter}
+      counter += 1;
+    }
+
+    // wywołaj funkcję --> plus()
+    plus();
+    plus();
+    plus();
+
+    // zwróć --> {counter}
+    return counter;
+  }
+}
+
+// ----------------------------
+// ---------------------------- Closures (and Self-invoking functions)
+// ----------------------------
+
+{
+  const add = (function () {
+    let counter = 0;
+    return function () {
+      counter += 1;
+      return counter;
+    };
+  })();
+
+  function myFunction() {
+    document.getElementById("demo_3").innerHTML = add();
+    // wywołaj metodę (funkcję) obiektu globalnego {WINDOW} przypisaną do add: (window.propertyName) --> add()
+  }
+}
+
+/* 
+The variable --add-- is assigned to the return value of a self-invoking function.
+
+The self-invoking function only runs once. It sets the counter to zero (0), and returns a function expression.
+
+This way add becomes a function. The "wonderful" part is that it can access the counter in the parent scope.
+
+This is called a JavaScript closure. It makes it possible for a function to have "private" variables.
+
+The counter is protected by the scope of the anonymous function, and can only be changed using the add function.
+*/
