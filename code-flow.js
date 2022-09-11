ff26ec567b314b7c1cd7443a051680878fdbe6e3
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-09-11 15:21:18 +0200

    CLOG

--- code-flow.js
+++ code-flow.js
@@ -1,4 +1,20 @@
-/* ---------------------------------------------------- */
+// DEMO_1 ----------------------------------------------
+// console.log configuration
+{
+  {
+    let x;
+
+    function myFunOne() {
+      x = 5;
+    }
+
+    // nie mam dostępu do x
+    console.log("LOG_1: nie mam dostępu do x --> : " + x);
+    myFunOne();
+    console.log("LOG_2 --> mam dostęp do x: " + x);
+  }
+}
+// code flow
 {
   let a = 4;
   let x = 5;
@@ -18,7 +34,7 @@
     methodOne: function () {
       document.getElementById("demo_2").innerHTML = a * a;
       x += 3;
-      console.log(this.name + " " + x);
+      console.log("LOG_3:  --> : " + this.name + " " + x);
       return x; // brak linii to consola nie będzie miała co zwrócić z funkcji do callera
     },
   };
@@ -27,14 +43,37 @@
   objectOne.methodOne();
 
   // pokaż co zwraca funkcja
-  console.log(objectOne.methodOne());
+  console.log("LOG_4:  --> : " + objectOne.methodOne());
+  console.log();
 }
-
-/* ---------------------------------------------------- */
-// HOISTING DECLARATION
+// DEMO_2 ----------------------------------------------
+// hoisting declaration
 {
   let a = 4;
   let x = 5;
   const objectCar = {};
   const arrayRecords = [];
+
+  function myFunOne(p1, p2) {
+    let x = p1;
+    let y = p2;
+    let z = p1 + p2;
+    document.getElementById("demo_3").innerHTML = "Hello Dolly";
+  }
 }
+// DEMO_3 ----------------------------------------------
+// counter dilema
+{
+  const add = (function () {
+    let counter = 0;
+    return function () {
+      counter += 1;
+      return counter;
+    };
+  })();
+
+  function myFunction() {
+    document.getElementById("demo_3").innerHTML = add();
+  }
+}
+//
