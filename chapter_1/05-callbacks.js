01ca9c3a203fc348db289a32828a9feab28caff6
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-09-14 10:46:10 +0200

    CALLBACKS

---
+++ chapter_1/05-callbacks.js
@@ -0,0 +1,67 @@
+// DEMO_1 ----------------------------------------------
+//  Functions are executed in the sequence they are called.
+{
+  console.log();
+  console.log("------------- DEMO_1 -------------");
+  console.log(" Functions are executed in the sequence they are called.");
+}
+{
+  function myDisplayer(some) {
+    document.getElementById("demo_1").innerHTML = some;
+  }
+
+  function myFirst() {
+    myDisplayer("Hello");
+  }
+
+  function mySecond() {
+    myDisplayer("Goodbye");
+  }
+
+  // CHANGE SEQUENCE OF CALLINGD
+  myFirst();
+  mySecond();
+}
+
+// DEMO_2 ----------------------------------------------
+// Do a calculation and then display the result FUNCTION
+
+{
+  console.log();
+  console.log("------------- DEMO_2 -------------");
+  console.log("Do a calculation and then display the result FUNCTION");
+}
+{
+  function myDisplayer(some) {
+    document.getElementById("demo_2").innerHTML = some;
+  }
+
+  function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+  }
+
+  let result = myCalculator(5, 5);
+
+  myDisplayer(result);
+}
+
+// DEMO_3 ----------------------------------------------
+// Do a calculation and then display the result CALLBACK
+{
+  console.log();
+  console.log("------------- DEMO_3 -------------");
+  console.log("Do a calculation and then display the result CALLBACK");
+}
+{
+  function myDisplayer(some) {
+    document.getElementById("demo_3").innerHTML = some;
+  }
+
+  function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    myDisplayer(sum);
+  }
+
+  myCalculator(5, 5);
+}
