ed3a9d706f4a08521691f8eb7bd67cedc8b93c08
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-09-16 17:55:01 +0200

    PROMISE

--- chapter_1/07-promises.js
+++ chapter_1/07-promises.js
@@ -10,16 +10,16 @@
     document.getElementById("demo_1").innerHTML = some;
   }
 
-  let myPromise = new Promise(function (myResolve, myReject) {
+  let myPromise = new Promise(function (myFunResolve, myFunReject) {
     let x = 5;
 
     // "Producing Code" (May take some time)
     //  (try to change x to 5)
 
-    if (x == 0) {
-      myResolve("OK");
+    if (x == 5) {
+      myFunResolve(x);
     } else {
-      myReject("Error - Something is wrong !!!");
+      myFunReject("x = " + x + " Error - Something is wrong !!!");
     }
   });
 
@@ -28,10 +28,12 @@
   myPromise.then(
     function (value) {
       myDisplayer(value);
+      console.log("LOG_1:  --> : " + value);
     },
 
     function (error) {
       myDisplayer(error);
+      console.log("LOG_2:  --> : " + error);
     }
   );
 }
@@ -42,27 +44,4 @@
   console.log();
   console.log("------------- DEMO_2 -------------");
   console.log("Waiting for a Timeout");
-}
-{
-  // CALLBACK
-
-  /*   setTimeout(function () {
-    myFunction("I love You !!!");
-  }, 3000);
-
-  function myFunction(value) {
-    document.getElementById("demo_2").innerHTML = value;
-  } */
-
-  //   PROMISE
-
-  const myPromise = new Promise(function (myResolve, myReject) {
-    setTimeout(function () {
-      myResolve("I love You !!");
-    }, 3000);
-  });
-
-  myPromise.then(function (value) {
-    document.getElementById("demo_2").innerHTML = value;
-  });
 }
